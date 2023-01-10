d6c64fb4cf279c0bd1f603a940e4acfbebf6e4fb
[mick lito] <[micklito.dev@gmail.com]>
2023-01-10 14:58:34 -0500

    added in remaining prompt commands to execute in series, added handleData function to multiply 10 to the power of choices.digitQuantity added letters variable to use in the future generator works in the case of y n n n but not outside of that

--- src/script.js
+++ src/script.js
@@ -1,10 +1,10 @@
 let choices = {
-  needNum: undefined, 
-  needLetter: undefined, 
+  needNum: undefined,
+  needLetter: undefined,
   needUpper: undefined,
   needSpecial: undefined,
-  digitQuantity: undefined
-}
+  digitQuantity: undefined,
+};
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -19,43 +19,76 @@ function writePassword() {
 
 function createEvent() {
   let num = prompt("Do you need numerical digits?", "y / n");
-  if (num != null) {
-     choices.needNum = num;
-     createSecondEvent()
+  if (num != null && (num == "y" || num == "n")) {
+    choices.needNum = num;
+    createSecondEvent();
   } else {
-    alert('invalid input, enter y / n');
+    alert("invalid input, enter y / n");
     createEvent();
   }
-};
+}
 
 function createSecondEvent() {
-  let letter = prompt('Do you need letter digits?', " y / n");
-  if (letter != null) {
-    choices.needLetter = letter
-    createFinalEvent()
+  let letter = prompt("Do you need letter digits?", " y / n");
+  if (letter != null && (letter == "y" || letter == "n")) {
+    choices.needLetter = letter;
+    createThirdEvent();
   } else {
-    alert('invalid input, enter y / n')
-    createSecondEvent()
+    alert("invalid input, enter y / n");
+    createSecondEvent();
+  }
+}
+
+function createThirdEvent() {
+  let upper = prompt("Do you need upper case letters?", " y / n");
+  if (upper != null && (upper == "y" || upper == "n")) {
+    choices.needUpper = upper;
+    createFourthEvent();
+  } else {
+    alert("invalid input, enter y / n");
+    createThirdEvent();
+  }
+}
+
+function createFourthEvent() {
+  let special = prompt("Do you need special characters?", " y / n");
+  if (special != null && (special == "y" || special == "n")) {
+    choices.needSpecial = special;
+    createFinalEvent();
+  } else {
+    alert("invalid input, enter y / n");
+    createFourthEvent();
   }
 }
 
 function createFinalEvent() {
-  let digit = prompt('How many digits do you need?', '8-128');
-  if (digit != null && digit >= 8 && digit <= 128) {
+  let digit = prompt("How long do would you like your password to be?", "8-20");
+  if (digit != null && digit >= 8 && digit <= 20) {
     choices.digitQuantity = digit;
-    generatePassword()
+    handleData();
   } else {
-    alert('Invalid input, please choose a number between 8 - 128');
-    createFinalEvent()
+    alert("Invalid input, please choose a number between 8 - 20");
+    createFinalEvent();
   }
 }
 
+function handleData() {
+  choices.digitQuantity = Math.pow(10, choices.digitQuantity);
+  generatePassword();
+}
 
 function generatePassword() {
-  if(choices.needNum == 'y' && choices.needLetter == 'n') {
-    const randomInt = Math.floor(Math.random() * 1000);
-    return randomInt}
+  const letters = ["abcdefghijklmnopqrstuvwxyz"];
+  const randomInt = Math.floor(Math.random() * choices.digitQuantity);
+  if (
+    choices.needNum == "y" &&
+    choices.needLetter == "n" &&
+    choices.needUpper == "n" &&
+    choices.needSpecial == "n"
+  ) {
+    return randomInt;
   }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+}
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
