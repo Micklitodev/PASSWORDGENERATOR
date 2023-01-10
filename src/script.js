81cc7bf93607bfaef58e05652ac7c617a7d9175a
[mick lito] <[micklito.dev@gmail.com]>
2023-01-10 17:16:32 -0500

    changed naming conventions of the event functions, added additional logic to letterfunction where if they input n it skips the upperEvent and assigns needUpper to n as it is a usless propmt if said no to letters. changed the generate function to have a password var and a nums var. conditional checks if need num was set by numEvent function as y, then the for loop executes the code in the amount of the value pushed into choices.digitQuantity by digitLengthEvent. resulting in password getting pushed a random number below 10 in the amount of times it exectues.

--- src/script.js
+++ src/script.js
@@ -1,9 +1,10 @@
+
 let choices = {
-  needNum: undefined,
-  needLetter: undefined,
-  needUpper: undefined,
-  needSpecial: undefined,
-  digitQuantity: undefined,
+  needNum: '',
+  needLetter: '',
+  needUpper: '',
+  needSpecial: '',
+  digitQuantity: undefined
 };
 
 // Assignment Code
@@ -11,84 +12,87 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  createEvent();
+  numsEvent()
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
-function createEvent() {
+ // Add event listener to generate button
+ generateBtn.addEventListener("click", writePassword);
+
+function numsEvent() {
   let num = prompt("Do you need numerical digits?", "y / n");
   if (num != null && (num == "y" || num == "n")) {
     choices.needNum = num;
-    createSecondEvent();
+    letterEvent();
   } else {
     alert("invalid input, enter y / n");
-    createEvent();
+    numsEvent();
   }
 }
 
-function createSecondEvent() {
+function letterEvent() {
   let letter = prompt("Do you need letter digits?", " y / n");
-  if (letter != null && (letter == "y" || letter == "n")) {
+  if (letter != null && (letter == "y")) {
     choices.needLetter = letter;
-    createThirdEvent();
-  } else {
+    upperEvent();
+  } else if (letter != null && (letter == "n")) {
+    choices.needUpper = 'no'
+    specialCharEvent()
+  } else  {
     alert("invalid input, enter y / n");
-    createSecondEvent();
+    letterEvent();
   }
 }
 
-function createThirdEvent() {
+function upperEvent() {
   let upper = prompt("Do you need upper case letters?", " y / n");
   if (upper != null && (upper == "y" || upper == "n")) {
     choices.needUpper = upper;
-    createFourthEvent();
+    specialCharEvent();
   } else {
     alert("invalid input, enter y / n");
-    createThirdEvent();
+    upperEvent();
   }
 }
 
-function createFourthEvent() {
+function specialCharEvent() {
   let special = prompt("Do you need special characters?", " y / n");
   if (special != null && (special == "y" || special == "n")) {
     choices.needSpecial = special;
-    createFinalEvent();
+    digitLengthEvent();
   } else {
     alert("invalid input, enter y / n");
-    createFourthEvent();
+    specialCharEvent();
   }
 }
 
-function createFinalEvent() {
+function digitLengthEvent() {
   let digit = prompt("How long do would you like your password to be?", "8-20");
   if (digit != null && digit >= 8 && digit <= 20) {
     choices.digitQuantity = digit;
-    handleData();
+    generatePassword();
   } else {
     alert("Invalid input, please choose a number between 8 - 20");
-    createFinalEvent();
+    digitLengthEvent();
   }
 }
 
-function handleData() {
-  choices.digitQuantity = Math.pow(10, choices.digitQuantity);
-  generatePassword();
-}
 
-function generatePassword() {
-  const letters = ["abcdefghijklmnopqrstuvwxyz"];
-  const randomInt = Math.floor(Math.random() * choices.digitQuantity);
-  if (
-    choices.needNum == "y" &&
-    choices.needLetter == "n" &&
-    choices.needUpper == "n" &&
-    choices.needSpecial == "n"
-  ) {
-    return randomInt;
+  function generatePassword() {
+    let password = '';
+    let nums = '0123456789';
+  
+    if (choices.needNum === 'y') {
+      for (let i = 0; i < choices.digitQuantity; i++) {
+        password += nums[Math.floor(Math.random() * nums.length)];
+      }
+    }
+    
+    return password;
   }
+  
 
-}
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+
+
